8818dffbb249fbe024a26a83d8f2d51ea40a7939
张佃磊 <532219308@qq.com>
2018-09-14 16:45:25 +0800

    修改judge判断方法之后，超时问题解决

--- src/c05.py
+++ src/c05.py
@@ -3,10 +3,28 @@
 @author: Dianlei Zhang
 @contact: dianlei.zhang@qq.com
 
-@time: 2018/9/14 15:05
+@time: 2018/9/14 13:10
 @python version: 
 
 """
+
+
+def judge(str):
+    mean = int((len(str) + 1) / 2)
+
+    for i in range(mean):
+        if str[i] != str[len(str) - 1 - i]:
+            return False
+    return True
+
+
+def judge2(str):
+
+    s2 = str[::-1]
+    if s2 == str:
+        return True
+    else:
+        return False
 
 
 class Solution:
@@ -16,8 +34,37 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        s2 = s[::-1]
+        result = ""
+        if len(s) == 0:
+            return result
+        else:
+            length = 0
 
+            for index, item in enumerate(s):
+
+                endIndex = s.rfind(item)
+
+                while endIndex != -1:
+                    tempStr = s[index:endIndex + 1]
+
+                    if judge2(tempStr):
+
+                        if (endIndex - index + 1) > length:
+                            length = endIndex - index + 1
+                            result = s[index:endIndex + 1]
+
+                            if length >= len(s) - index:
+                                return result
+                            break
+
+                    endIndex = s.rfind(item, 0, endIndex)
+
+                if endIndex == -1:
+                    if length == 0:
+                        result = item
+                        length = 1
+
+            return result
 
 
 def stringToString(input):
