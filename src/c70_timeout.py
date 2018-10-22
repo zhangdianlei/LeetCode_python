1a96fe38178818eab2761b32f5be8d2dc0b23fba
张佃磊 <532219308@qq.com>
2018-10-22 14:18:36 +0800

    70爬楼梯，其实就是斐波那契数列数列

---
+++ src/c70_timeout.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+"""
+@author: Dianlei Zhang
+@contact: dianlei.zhang@qq.com
+
+@time: 2018/10/22 13:52
+@python version: 
+
+"""
+
+
+def p(n):
+    if n == 2:
+        return 2
+    if n == 3:
+        return 3
+    return p(n - 1) + p(n - 2)
+
+
+def climbStairs(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 3
+
+    result = p(n - 1) + p(n - 2)
+    return result
+
+
+if __name__ == '__main__':
+    n = 2
+    print(climbStairs(n))
