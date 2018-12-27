098d6c4a86294920f91df700dea319e45b18616b
zhangdianlei <dianlei.zhang@gmail.com>
2018-12-27 16:56:28 +0800

    154 寻找旋转排序数组中的最小值 II

--- src/part2/c154.py
+++ src/part2/c154.py
@@ -11,6 +11,9 @@ def findMin(nums, init):
     :type nums: List[int]
     :rtype: int
     """
+    if len(nums) == 1:
+        return nums[0]
+
     result = init
     if nums[0] <= nums[-1]:
         if nums[0] <= result:
@@ -24,5 +27,21 @@ def findMin(nums, init):
 if __name__ == '__main__':
     # nums = [2, 2, 2, 0, 1]
     nums = [3, 1, 3]
+    nums = [1, 3, 5]
+    nums = [1, 1, 1]
 
-    print(findMin(nums, 99999999))
+    pre = nums[0]
+    new = [pre]
+    for i in range(1, len(nums)):
+        if nums[i] != pre:
+            new.append(nums[i])
+            pre = nums[i]
+
+    if len(new) > 1:
+        if new[0] == new[-1]:
+            new = new[1:]
+    else:
+        # return nums[0]
+        print(nums[0])
+
+    print(findMin(new, 99999999))
