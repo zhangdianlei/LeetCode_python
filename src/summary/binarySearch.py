9c5abea9d1525f93c3da47208950a77f2a6cbe8c
zhangdianlei <dianlei.zhang@gmail.com>
2019-01-05 19:36:44 +0800

    二分查找

--- src/summary/binarySearch.py
+++ src/summary/binarySearch.py
@@ -7,14 +7,21 @@
 
 
 def binarySearch(A, target):
+
     start = 0
     end = len(A) - 1
+
+    # 循环判断，直到找到target或者是start > end
     while start <= end:
         mid = (start + end) / 2
+
+        # 返回结果
         if A[mid] == target:
             return mid
+        # 舍弃掉左半部分
         if A[mid] < target:
             start = mid + 1
+        # 舍弃掉右半部分
         else:
             end = mid - 1
 
